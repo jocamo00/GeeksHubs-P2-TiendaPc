a5bbccf2dfc4dbe6a1aeb83893b6daf20a656e7b
Jose <jose.carreres.81@gmail.com>
2020-03-08 11:16:17 +0100

    Desarrollando funcionalidad drag and drop en productos

--- js/index.js
+++ js/index.js
@@ -122,15 +122,16 @@ var Ordenador = [
         hd:"250GB SSD",
         precio:"349€",
         valoracion:"★"
-    }, 
+    }
 ]
+
 
 let val;
 
 for(let i = 0; i<Ordenador.length; i++){
 
-    document.getElementById("productos").innerHTML +=
-    '<div id="elemento'+[i]+'" class="container-producto col-sm-6 col-lg-4 col-xl-3">'+
+    document.getElementById("productos").innerHTML += '<div id="elemento'+[i]+'" class="container-producto col-sm-6 col-lg-4 col-xl-3">'+
+        //'<button id="btnFavorito'+[i]+'" onclick="favoritos()">favoritos</button>'+
         '<img class="d-block w-100" src='+Ordenador[i].img+' alt="">'+
         '<div class="contenedor-info">'+
             '<div class="contenedorMarca">'+
@@ -148,36 +149,106 @@ for(let i = 0; i<Ordenador.length; i++){
             
                 for(let k = 0; k < 5; k++){
                     document.getElementById("estrella"+[i]).innerHTML +=
-                        '<input id="radio'+i+k+'" type="radio" name="estrellas" value="'+k+'" onclick="myFunction()">'+
+                        '<input id="radio'+i+k+'" type="radio" name="estrellas" value="'+(1+k)+'">'+
                         '<label for="radio'+i+k+'">★</label>'
 
-                        function myFunction() {
+                        /* function myFunction() {
                             let r = document.getElementById("radio"+i+k);
                             console.log(r)
                             //console.log(r.getAttribute("value"));
                             //val = r.getAttribute("value");
                             //let suma = Number(val) +100;
                             //console.log(val);
-                        }
+                        } */
+
+                        /* function myFunction() {
+                            let radio = document.getElementById("radio"+[i]+[k]);
+                            console.log(r);
+                        } */
+                        
+
+
+                        
 
                         /* let r = document.getElementById("radio"+i+k);
                         val = r.getAttribute("value"); */
-            
+                }
             '</div>'+
         '</div>'+   
     '</div>'; 
-                }  
-
 }
 
-                    /* let suma = Number(val) +100;
-                    console.log(suma); */
+ 
+
+
+
+/* let favoritos = [];
+let elemento = document.getElementById("elemento0");
+
+
+favoritos.push(elemento);
+
+console.log(favoritos); */
+/* document.getElementById("favoritos").innerHTML += '<div id="jose"></div>'; */
+
+//console.log(elemento);
 
 
 
 
+var container;
+     
+        function init(){
+            container = document.querySelector('.carrito');
+            container.addEventListener('dragover', dragSobreContainer, false);
+            container.addEventListener('dragleave', dragSalioContainer, false);
+            container.addEventListener('drop', manejarDrop, false);
+
+            var container_producto = document.getElementsByClassName('container-producto');
+
+            for(i in container_producto){
+                var contProducto = container_producto[i];
+                contProducto.addEventListener('dragstart', dragIniciado, false);
+                contProducto.addEventListener('dragend', dragFinalizado,false);
+
+                }
+            }
+        
+
+        init();
 
 
+        function dragIniciado(e){
+            //this.style.backgroundColor='blue';
+            var padre = document.createElement('div');
+            var clon = this.cloneNode(true);
+            padre.appendChild(clon);
+            e.dataTransfer.setData('text', padre.innerHTML);
+        }
+
+        function manejarDrop(e){
+            e.preventDefault();
+            var datos = e.dataTransfer.getData('text');
+            this.innerHTML += datos;
+
+        }
+
+        function dragSobreContainer(e){
+            e.preventDefault();
+            this.classList.add('over');
+            return false;
+
+        }
+
+        function dragSalioContainer(e){
+            this.classList.remove('over');
+        }
+
+        function dragFinalizado(e){
+            //this.style.backgroundColor='red';
+            
+            
+        }
 
 
 
